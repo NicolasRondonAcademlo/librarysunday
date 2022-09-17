77cb2951e902bd6c0d839c60e8c7ccfb842a068b
NicoRondonData <nrondon@ucompensar.edu.co>
2022-09-17 12:04:24 -0500

    mny

--- library_api/books/urls.py
+++ library_api/books/urls.py
@@ -1,10 +1,17 @@
 from rest_framework.routers import DefaultRouter
 
-from .views import AuthorViewSet, BookItemUserViewSet, BookViewSet, CategoryViewSet
+from .views import (
+    AuthorViewSet,
+    BookItemUserViewSet,
+    BookItemViewSet,
+    BookViewSet,
+    CategoryViewSet,
+)
 
 router = DefaultRouter()
 router.register("categories", CategoryViewSet)
 router.register("books", BookViewSet)
 router.register("authors", AuthorViewSet)
 router.register("book_users", BookItemUserViewSet)
+router.register("book_items", BookItemViewSet)
 urlpatterns = [] + router.urls
--- library_api/books/views.py
+++ library_api/books/views.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from rest_framework import status, viewsets
+from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from .models import Author, Book, BookItem, BookItemUser, Category
@@ -36,20 +38,51 @@ class BookItemViewSet(viewsets.ModelViewSet):
     serializer_class = BookItemSerializer
     queryset = BookItem.objects.all()
 
+    @action(detail=False)
+    def all_book_items_rent(self, request):
+        book_items = BookItem.objects.filter(is_rent=True)
+        if book_items.count() == 0:
+            return Response({"data": "Not Found"}, status=status.HTTP_404_NOT_FOUND)
+        serializer = BookItemSerializer(
+            book_items, many=True, context={"request": request}
+        )
+        return Response({"data": serializer.data})
+
 
 class BookItemUserViewSet(viewsets.ModelViewSet):
     # permission_classes = [IsLibrarianOrReadOnly]
     serializer_class = BookItemUserSerializer
     queryset = BookItemUser.objects.all()
 
+    def __find_numbers_of_rent_books_by_user(self, user_id):
+        total_books = BookItemUser.objects.filter(user__id=user_id).count()
+        return total_books
+
     def create(self, request, *args, **kwargs):
         serializer = BookItemUserSerializerCreate(data=request.data)
         if serializer.is_valid():
+            number_books = self.__find_numbers_of_rent_books_by_user(
+                request.data["user"]
+            )
+            if number_books >= settings.MAX_BOOKS_TO_RENT:
+                return Response(
+                    {
+                        "data": f"Actualmente tienes {number_books} libros rentados y el maximo es {settings.MAX_BOOKS_TO_RENT}"
+                    },
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
             book_item = BookItem.objects.get(pk=request.data["book_item"])
+            if book_item.is_rent:
+                return Response(
+                    {"data": "El libro ya esta rentado"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
             book_item.is_rent = True
             book_item.save()
             serializer.save()
-            return Response({"data": serializer.data}, status=status.HTTP_200_OK)
+            book_user = BookItemUser.objects.get(pk=serializer.data["id"])
+            data = BookItemUserSerializer(book_user, context={"request": request})
+            return Response({"data": data.data}, status=status.HTTP_200_OK)
         else:
             return Response(
                 {"data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST
